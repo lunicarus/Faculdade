b5c769bb8e69a6fc5ba190d47f17e0057d6489fa
lucas da silva cruz <lucas6jau@gmail.com>
2023-05-24 20:21:28 -0300

    usando listas

--- projeto.py
+++ projeto.py
@@ -37,134 +37,128 @@ def submenu_sessoes():
 # Função para exibir o submenu de relatórios
 def submenu_relatorios():
     print("Submenu de Relatórios:")
-    print("a) Mostrar todos os dados de todas as salas cujo tipo de exibição seja X e capacidade para mais de Y pessoas")
-    print("b) Mostrar todos os dados de todos os filmes que foram lançados a partir do ano X")
-    print("c) Mostrar os dados das sessões exibidas a partir de uma data inicial X até uma data final Y")
+    print("1. Mostrar todas as salas com tipo de exibição X e capacidade maior que Y")
+    print("2. Mostrar todos os filmes lançados a partir do ano X")
+    print("3. Mostrar todas as sessões exibidas entre as datas X e Y")
 
-# Função para exibir relatório de salas
-def relatorio_salas(salas, tipo_exibicao, capacidade_minima):
-    print("Relatório de Salas:")
-    for sala in salas:
-        if sala['Tipo de exibição'] == tipo_exibicao and sala['Capacidade'] > capacidade_minima:
-            print(f"Código: {sala['Código']}")
-            print(f"Nome: {sala['Nome']}")
-            print(f"Capacidade: {sala['Capacidade']}")
-            print(f"Tipo de exibição: {sala['Tipo de exibição']}")
-            print(f"Acessível: {sala['Acessível']}")
-            print()
+# Função para listar todas as salas com tipo de exibição X e capacidade maior que Y
+def relatorio_salas_tipo_capacidade(salas, tipo_exibicao, capacidade):
+    salas_encontradas = []
+    for sala in salas.values():
+        if sala["tipo_exibicao"] == tipo_exibicao and sala["capacidade"] > capacidade:
+            salas_encontradas.append(sala)
+    if len(salas_encontradas) == 0:
+        print("Nenhuma sala encontrada com os critérios especificados.")
+    else:
+        print("Salas encontradas:")
+        for sala in salas_encontradas:
+            exibir_dados_sala(sala)
+            print("")
 
-# Função para exibir relatório de filmes
-def relatorio_filmes(filmes, ano_lancamento):
-    print("Relatório de Filmes:")
-    for filme in filmes:
-        if filme['Ano de lançamento'] >= ano_lancamento:
-            print(f"Código: {filme['Código']}")
-            print(f"Nome: {filme['Nome']}")
-            print(f"Ano de lançamento: {filme['Ano de lançamento']}")
-            print(f"Diretor: {filme['Diretor']}")
-            print(f"Atores: {', '.join(filme['Atores'])}")
+# Função para listar todos os filmes lançados a partir do ano X
+def relatorio_filmes_lancados(filmes, ano):
+    filmes_encontrados = []
+    for filme in filmes.values():
+        if filme["ano_lancamento"] >= ano:
+            filmes_encontrados.append(filme)
+    if len(filmes_encontrados) == 0:
+        print("Nenhum filme encontrado com os critérios especificados.")
+    else:
+        print("Filmes encontrados:")
+        for filme in filmes_encontrados:
+            exibir_dados_filme(filme)
+            print("")
 
-# Função para exibir relatório de sessões
-def relatorio_sessoes(sessoes, data_inicial, data_final):
-    print("Relatório de Sessões:")
-    for sessao in sessoes:
-        if data_inicial <= sessao['Data'] <= data_final:
-            print(f"Código do Filme: {sessao['Código do Filme']}")
-            print(f"Código da Sala: {sessao['Código da Sala']}")
-            print(f"Data: {sessao['Data']}")
-            print(f"Horário: {sessao['Horário']}")
-            print(f"Preço do Ingresso: {sessao['Preço do Ingresso']}")
-            print()
+# Função para listar todas as sessões exibidas entre as datas X e Y
+def relatorio_sessoes_data(sessoes, data_inicial, data_final):
+    sessoes_encontradas = []
+    for sessao in sessoes.values():
+        if data_inicial <= sessao["data"] <= data_final:
+            sessoes_encontradas.append(sessao)
+    if len(sessoes_encontradas) == 0:
+        print("Nenhuma sessão encontrada com os critérios especificados.")
+    else:
+        print("Sessões encontradas:")
+        for sessao in sessoes_encontradas:
+            exibir_dados_sessao(sessao)
+            print("")
 
-# Função principal
+# Função para exibir os dados de uma sala
+def exibir_dados_sala(sala):
+    print("Código:", sala["codigo"])
+    print("Nome:", sala["nome"])
+    print("Capacidade:", sala["capacidade"])
+    print("Tipo de Exibição:", sala["tipo_exibicao"])
+    print("Acessível:", sala["acessivel"])
+
+# Função para exibir os dados de um filme
+def exibir_dados_filme(filme):
+    print("Código:", filme["codigo"])
+    print("Nome:", filme["nome"])
+    print("Ano de Lançamento:", filme["ano_lancamento"])
+    print("Diretor:", filme["diretor"])
+    print("Atores:", ", ".join(filme["atores"]))
+
+# Função para exibir os dados de uma sessão
+def exibir_dados_sessao(sessao):
+    print("Código do Filme:", sessao["codigo_filme"])
+    print("Código da Sala:", sessao["codigo_sala"])
+    print("Data:", sessao["data"])
+    print("Horário:", sessao["horario"])
+    print("Preço do Ingresso:", sessao["preco_ingresso"])
+
+# Função principal do programa
 def main():
-    salas = []
-    filmes = []
-    sessoes = []
+    salas = {}
+    filmes = {}
+    sessoes = {}
 
     while True:
         exibir_menu()
         opcao = input("Escolha uma opção: ")
 
-        if opcao == "1":  # Submenu de Salas
+        if opcao == "1":
             submenu_salas()
-            opcao_salas = input("Escolha uma opção: ")
+            # Implemente as operações do submenu de salas
+            # Listar todas, Listar uma, Incluir, Alterar, Excluir
 
-            if opcao_salas == "1":  # Listar todas as salas
-                print("Listar todas as salas")
-
-            elif opcao_salas == "2":  # Listar uma sala específica
-                print("Listar uma sala específica")
-
-            elif opcao_salas == "3":  # Incluir uma sala
-                print("Incluir uma sala")
-
-            elif opcao_salas == "4":  # Alterar uma sala
-                print("Alterar uma sala")
-
-            elif opcao_salas == "5":  # Excluir uma sala
-                print("Excluir uma sala")
-
-        elif opcao == "2":  # Submenu de Filmes
+        elif opcao == "2":
             submenu_filmes()
-            opcao_filmes = input("Escolha uma opção: ")
+            # Implemente as operações do submenu de filmes
+            # Listar todos, Listar um, Incluir, Alterar, Excluir
 
-            if opcao_filmes == "1":  # Listar todos os filmes
-                print("Listar todos os filmes")
-
-            elif opcao_filmes == "2":  # Listar um filme específico
-                print("Listar um filme específico")
-
-            elif opcao_filmes == "3":  # Incluir um filme
-                print("Incluir um filme")
-
-            elif opcao_filmes == "4":  # Alterar um filme
-                print("Alterar um filme")
-
-            elif opcao_filmes == "5":  # Excluir um filme
-                print("Excluir um filme")
-
-        elif opcao == "3":  # Submenu de Sessões
+        elif opcao == "3":
             submenu_sessoes()
-            opcao_sessoes = input("Escolha uma opção: ")
+            # Implemente as operações do submenu de sessões
+            # Listar todas, Listar uma, Incluir, Alterar, Excluir
 
-            if opcao_sessoes == "1":  # Listar todas as sessões
-                print("Listar todas as sessões")
-
-            elif opcao_sessoes == "2":  # Listar uma sessão específica
-                print("Listar uma sessão específica")
-
-            elif opcao_sessoes == "3":  # Incluir uma sessão
-                print("Incluir uma sessão")
-
-            elif opcao_sessoes == "4":  # Alterar uma sessão
-                print("Alterar uma sessão")
-
-            elif opcao_sessoes == "5":  # Excluir uma sessão
-                print("Excluir uma sessão")
-
-        elif opcao == "4":  # Submenu de Relatórios
+        elif opcao == "4":
             submenu_relatorios()
-            opcao_relatorios = input("Escolha uma opção: ")
+            relatorio_opcao = input("Escolha uma opção de relatório: ")
 
-            if opcao_relatorios == "a":  # Relatório de salas
-                tipo_exibicao = input("Digite o tipo de exibição: ")
-                capacidade_minima = int(input("Digite a capacidade mínima: "))
-                relatorio_salas(salas, tipo_exibicao, capacidade_minima)
+            if relatorio_opcao == "1":
+                tipo_exibicao = input("Digite o tipo de exibição (X): ")
+                capacidade = int(input("Digite a capacidade mínima (Y): "))
+                relatorio_salas_tipo_capacidade(salas, tipo_exibicao, capacidade)
 
-            elif opcao_relatorios == "b":  # Relatório de filmes
-                ano_lancamento = int(input("Digite o ano de lançamento: "))
-                relatorio_filmes(filmes, ano_lancamento)
+            elif relatorio_opcao == "2":
+                ano = int(input("Digite o ano mínimo (X): "))
+                relatorio_filmes_lancados(filmes, ano)
 
-            elif opcao_relatorios == "c":  # Relatório de sessões
-                data_inicial = input("Digite a data inicial (formato: dd/mm/aaaa): ")
-                data_final = input("Digite a data final (formato: dd/mm/aaaa): ")
-                relatorio_sessoes(sessoes, data_inicial, data_final)
+            elif relatorio_opcao == "3":
+                data_inicial = input("Digite a data inicial (formato: DD/MM/AAAA): ")
+                data_final = input("Digite a data final (formato: DD/MM/AAAA): ")
+                relatorio_sessoes_data(sessoes, data_inicial, data_final)
 
-        elif opcao == "5":  # Sair
+            else:
+                print("Opção inválida.")
+
+        elif opcao == "5":
+            print("Saindo do programa...")
             break
 
-        print()
+        else:
+            print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     main()
