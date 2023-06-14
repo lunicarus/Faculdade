0200475a0896de98e80e84110c772c387f7b6732
lucas da silva cruz <lucas6jau@gmail.com>
2023-06-13 21:20:34 -0300

    tarefa

--- python/matrizes_vetores.py
+++ python/matrizes_vetores.py
@@ -1,142 +1,61 @@
-def ex1():
-    matriz=[]
-
-    N=int(input("Informe a quantidade de linhas: "))
-    M=int(input("Informe a quantidade de colunas: "))
+def montaMatriz(M=0,N=0):
+    matriz = []
+    if M == 0 or N == 0:
+        print("informe as dimensões da matriz (N,M)")
+        N= int(input())
+        M= int(input())
 
     for i in range(N):
-        linha=[]
+        linha = []
         for j in range(M):
-            elem=int(input(f"Digite o elemento da posição ({i},{j}): "))
+            elem = int(input(f"digite o elemento ({i},{j})"))
             linha.append(elem)
         matriz.append(linha)
-    maior=matriz[0][0]
-    menor=matriz[0][0]
+    return matriz
 
-    for l in matriz:
-        for elem in linha:
-            if elem>maior:
-                maior=elem
-            if elem<menor:
-                menor=elem
-            
-    print("O maior valor é: ", maior)
-    print("O menor valor é: ", menor)
-def ex2():
-    matriz=[]
-    n=int(input("Informe a dimensão da matriz A quadrada: "))
-    for i in range(n):
-        linha=[]
-        for j in range(n):
-            elem=int(input(f"Digite o elemento da posição ({i},{j}): "))
-            linha.append(elem)
-        matriz.append(linha)
-    matrizt=[]
-    for i in range(n):
-        linha = []
-        for j in range(n):
-            linha.append(matriz[j][i])
-        matrizt.append(linha)
+def MaiorMenor(matriz):
+        maior = matriz[0][0]
+        menor = matriz[0][0]
 
+        for linha in matriz:
+            for elemento in linha:
+                if elemento > maior:
+                    maior = elemento
+                if elemento < menor:
+                    menor = elemento
+        return f"maior : {maior} || menor : {menor}"
 
-    print("Matriz A:")
-    for linha in matriz:
-        print(linha)
-    
-    print("Matriz At:")
-    for linha in matrizt:
-        print(linha)
-def ex3():     
-    matriz=[]
-    n=int(input("Informe a dimensão da matriz A quadrada: "))
-    #criando a matriz original
-    for i in range(n):
-        linha=[]
-        for j in range(n):
-            elem=int(input(f"Digite o elemento da posição ({i},{j}): "))
-            linha.append(elem)
-        matriz.append(linha)
-    
-    matrizt=[]
-    for i in range(n):
-        linha = []
-        for j in range(n):
-            linha.append(matriz[j][i])
-        matrizt.append(linha)
-
-
-    print("Matriz A:")
-    for linha in matriz:
-        print(linha)
-    
-    print("Matriz At:")
-    for linha in matrizt:
-        print(linha)
-    
-    if matriz==matrizt:
-        print("A matriz é simetrica")
-    else:
-        print("Esta matriz não é simetrica")
-def ex4():
-    a=[]
-
-
-    n=int(input("Informe a quantidade de linhas: "))
-    m=int(input("Informe a quantidade de colunas: "))
-
-
-    for i in range(n):
-        linha=[]
-        for j in range(m):
-            elem=int(input(f"Digite o elemento da posição ({i},{j}): "))
-            linha.append(elem)
-        a.append(linha)
-
-
-    linnul=0
-    for linha in a:
-        if sum(linha)==0:
-            linnul+=1
-
-
-    colnul = 0
-    for j in range(n):
-        soma_coluna = 0
-        for i in range(m):
-            soma_coluna += a[i][j]
-        if soma_coluna == 0:
-            colnul += 1
-        
-    print("Quantidade de linhas nulas: ", linnul)
-    print("Quantidade de colunas nulas: ", colnul)
-def ex5():
-    m = int(input("Digite a quantidade de linhas da matriz 1: "))
-    n = int(input("Digite a quantidade de colunas da matriz 1: "))
-    p = int(input("Digite a quantidade de linhas da matriz 2: "))
-    q = int(input("Digite a quantidade de colunas da matriz 2: "))
-
-
-    if n != p:
-        print("Não é possível realizar a multiplicação das matrizes.")
-    else:
-        matriz1 = []
-        for i in range(m):
+def montaMatrizT(matriz,n):
+        matrizt = []
+        for i in range(n):
             linha = []
             for j in range(n):
-                elemento = int(input(f"Digite o elemento da posição ({i},{j}) da matriz 1: "))
-                linha.append(elemento)
-            matriz1.append(linha)
+                linha.append(matriz[j][i])
+            matrizt.append(linha)
+        return matrizt
+# [0,1,2]
+# [0,2,3]
+# [0,0,0]
+##
+def elementosNulos(matriz,n,m): 
+        Lnulos = 0
+        Cnulos = 0
 
+        for linha in matriz: #[0,1,2] 
+            if sum(linha) == 0:
+                Lnulos +=1
+        Lnulos = 0
 
-        matriz2 = []
-        for i in range(p):
-            linha = []
-            for j in range(q):
-                elemento = int(input(f"Digite o elemento da posição ({i},{j}) da matriz 2: "))
-                linha.append(elemento)
-            matriz2.append(linha)
-
-
+        for j in range(n):
+            soma_coluna = 0
+        for i in range(m):
+            soma_coluna += matriz[i][j]
+        if soma_coluna == 0:
+            Lnulos += 1
+        
+        print("Quantidade de linhas nulas: ", Lnulos)
+        print("Quantidade de colunas nulas: ", Cnulos)
+def produtomatriz(matriz1,matriz2,m,n,q):
         matrizp = []
         for i in range(m):
             linha = []
@@ -146,14 +65,49 @@ def ex5():
                     soma += matriz1[i][k] * matriz2[k][j]
                 linha.append(soma)
             matrizp.append(linha)
+        print(f"Matriz 1: {matriz1}")
+        print(f"Matriz 2: {matriz2}")
+        print(f"Matriz Produto: {matrizp}")
 
 
-        print("Matriz 1:")
-        for linha in matriz1:
-            print(linha)
-        print("Matriz 2:")
-        for linha in matriz2:
-            print(linha)
-        print("Matriz Produto:")
-        for linha in matrizp:
-            print(linha)
+        
+            
+            
+
+
+def ex1():
+    print(MaiorMenor(montaMatriz()))
+
+def ex2():
+    n=int(input("Informe a dimensão da matriz A quadrada: "))
+    
+    matriz= montaMatriz(n,n)
+    matrizt= montaMatrizT(matriz,n)
+    print(f"matriz: {matriz} \nmatrizT: {matrizt}")
+
+def ex3():     
+    n=int(input("Informe a dimensão da matriz A quadrada: "))
+    matriz= montaMatriz(n,n)
+    matrizt= montaMatrizT(matriz,n)
+    print(f"matriz: {matriz} \nmatrizT: {matrizt}")
+    if matriz==matrizt:
+        print("A matriz é simetrica")
+    else:
+        print("Esta matriz não é simetrica")
+
+def ex4():
+    n= int(input())
+    m= int(input())
+    matriz= montaMatriz(n,m)
+    elementosNulos(matriz,n,m)
+def ex5():
+    m = int(input("Digite a quantidade de linhas da matriz 1: "))
+    n = int(input("Digite a quantidade de colunas da matriz 1: "))
+    p = int(input("Digite a quantidade de linhas da matriz 2: "))
+    q = int(input("Digite a quantidade de colunas da matriz 2: "))
+    matriz1 = montaMatriz(m,n)
+    matriz2 = montaMatriz(p,q)
+    if n != p:
+        print("Não é possível realizar a multiplicação das matrizes.")
+    else:
+        produtomatriz(matriz1,matriz2,m,n,q)
