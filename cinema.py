7c6012da497ac6e347b5f7f6bee419e322825048
Isabela de Ponte <isabelapnt01@gmail.com>
2023-06-23 12:30:45 -0300

    mais uma versao quase final

--- cinema.py
+++ cinema.py
@@ -296,6 +296,7 @@ def ExcluirSala(salas,sessoes):
 
 #region Sessões
 
+
 def SubmenuSessoes(sessoes,filmes,salas):
     escolhaMenuSessao = '0'
     while escolhaMenuSessao != '6':
@@ -355,6 +356,7 @@ def IncluirSessao(filmes,salas,sessoes):
     while codFilme not in filmes:
         print("Código de filme inexistente, digite novamente")
         codFilme = int(input("Digite o código do filme: "))
+ 
     def HoraData():
         Horario = ''
         while not Horario:
